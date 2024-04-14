61ca77a0dad7e86cea14095f952c6a2acf36148b
Valeria Jarquin <vjarquin99@gmail.com>
2024-04-13 22:30:11 -0600

    Solucion del ejercicio4 + archivo creado de log + .service

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 # Valeria Jarquin Vargas, B83986
-# Uso: ./ejercicio4.sh 
+# Uso: ./ejercicio4.sh
+
+# Se crea una variable que guarde el path del directorio a monitorear
+directorio=/var/log
+
+# Se crea un bucle para monitorear el directorio constantemente
+while true; do
+    # Argumento para guardar un evento
+    evento=$(inotifywait -q -e create,modify,delete -r "$directorio" --format "%e %w%f")
+    if [ $? -eq 0 ]; then
+        # Para obtener la fecha y hora del cambio
+        fecha_hora=$(date +"%Y-%m-%d %H:%M:%S")
+        # Para escribir el evento
+        echo "$fecha_hora - Cambio detectado: $evento" >> /home/vjarquin/Plataformas/Laboratorio2/fecha_hora_evento.txt
+    fi
+done
